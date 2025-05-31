23379be83fdacee4c6b71117a265fb3f74ce6b59
EliasFritioff <142496350+EliasFritioff@users.noreply.github.com>
2025-05-31 21:53:14 +0200

    So

--- quiz.py
+++ quiz.py
@@ -1,4 +1,6 @@
+# quiz.py
+
 import os
-os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\Programmering 2\\Slut-Uppgift\\quiz && python server.py"')
-os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\Programmering 2\\Slut-Uppgift\\quiz && python client.py"')
-os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\Programmering 2\\Slut-Uppgift\\quiz && python client.py"')
+os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\quiz && python server.py"')
+os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\quiz && python client.py"')
+os.system('start cmd /K "cd /d C:\\Users\\elias.fritioff\\quiz && python client.py"')
